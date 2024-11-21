6b52c6f077a851bc8abbe5f735c4ab474a5bdcab
Ömer <okozluca.1@gmail.com>
2024-11-21 16:06:11 +0300

    Add files via upload

---
+++ BasicMvcProject/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,38 @@
+﻿using BasicMvcProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicMvcProject.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            var customer1 = new Customer
+            {
+                Id = 1,
+                Name = "Ömer",
+                SurName = "Kozluca",
+                Email = "Omer@hotmail.com"
+
+            };
+
+            var orders = new List<Order>()
+            {
+                new Order {Id = 1, ProductName ="Ford", Price=27500, Quantity= 2 },
+                new Order {Id = 2, ProductName ="TOYOTA", Price=37250, Quantity= 2 },
+                new Order {Id = 3, ProductName ="FIAT", Price=14320, Quantity= 2 }
+            };
+
+            var viewModel = new CustomerOrderViewModel()
+            {
+                Customer = customer1,
+                Orders = orders
+            };
+
+
+            return View(viewModel);
+
+        }
+    }
+}
